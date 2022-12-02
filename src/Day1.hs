f0981c85f505acc3d62ea9043e7f1bea91ddf1ce
Alessandro Candolini <alessandrocandolini@gmail.com>
2022-12-02 17:21:15 +0000

    ugly implementation

--- src/Day1.hs
+++ src/Day1.hs
@@ -1,12 +1,17 @@
 {-# LANGUAGE DerivingVia #-}
-module Day1(program) where
+module Day1 where
 
 import qualified Data.Text as T
 import Data.Text.IO as T
 import Control.Monad
 import Data.List.NonEmpty
 import Data.Sequence.NonEmpty (sortOn)
-import Prelude hiding (head)
+import Prelude hiding (reverse, head)
+import Control.Exception
+import UnliftIO
+import Data.Either.Combinators (maybeToRight)
+import Data.List.Split
+import Data.List.Index
 
 program :: FilePath -> IO ()
 program = longStory
@@ -25,24 +30,37 @@ newtype ElfName = ElfName Int deriving (Eq,Show)
    deriving (Num) via Int
 
 newtype Calories = Calories Int deriving (Eq,Show, Ord)
-   deriving (Num) via Int
+   deriving (Num, Read) via Int
 
+data ParseFail = ParseFail deriving (Eq,Show)
+
+instance Exception ParseFail 
+  
 data Elf = Elf {
    name :: ElfName,
    calories :: [Calories]
    } deriving (Eq,Show)
 
 fetchElves :: FilePath -> IO (NonEmpty Elf)
-fetchElves = undefined
+fetchElves = fromEitherIO . fmap (maybeToRight ParseFail . parse) . T.readFile 
+
+parse :: T.Text -> Maybe (NonEmpty Elf) 
+parse =  nonEmpty . fmap parseElf . indexed . splitWhen null . fmap T.unpack . T.lines  where 
+
+   parseElf :: (Int, [String]) -> Elf  -- ignore errors 
+   parseElf (i,c) = Elf (ElfName (i +1)) (fmap read c )
+
+
 
 totalCalories :: Elf -> Calories
 totalCalories = sum . calories
 
 logic :: NonEmpty Elf -> Elf
-logic = head . sortWith totalCalories
+logic = head . reverse . sortWith totalCalories
 
 printResults :: Elf -> IO ()
 printResults e = (T.putStrLn . T.pack) $ show (name e) ++ "\n" ++ show (totalCalories e)
+
 
 
 
--- test/Day1Spec.hs
+++ test/Day1Spec.hs
@@ -1,15 +1,57 @@
+{-# LANGUAGE QuasiQuotes #-}
 module Day1Spec where
 
 import Day1
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck.Property
+import NeatInterpolation
+import Data.Text (Text)
+import qualified Data.List.NonEmpty as N
 
 spec :: Spec
 spec = describe "Simple test" $ do
 
-     it "example-based unit test" $
-        1 `shouldBe` 1
+     it "parse elves" $
+        parse [trimming| 
+          1000
+          2000
+          3000
+
+          4000
+
+          5000
+          6000
+
+          7000
+          8000
+          9000
+
+          10000
+          |] `shouldBe` Just [
+               Elf 1 [1000, 2000, 3000],
+               Elf 2 [4000],
+               Elf 3 [5000, 6000],
+               Elf 4 [7000, 8000, 9000],
+               Elf 5 [10000]
+            ]
+     it "calculate result" $
+        (fmap logic . parse) [trimming| 
+          1000
+          2000
+          3000
+
+          4000
+
+          5000
+          6000
+
+          7000
+          8000
+          9000
+
+          10000
+          |] `shouldBe` Just (Elf 4 [7000, 8000, 9000])
 
      prop "property-based unit test" $
         \l -> reverse ( reverse l ) == ( l::[Int])
