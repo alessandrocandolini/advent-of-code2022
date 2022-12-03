46d0058200faa9cf5f403ec6d87528aa7470d78e
Alessandro Candolini <alessandrocandolini@gmail.com>
2022-12-03 21:07:35 +0000

    wip day2

--- src/Day2.hs
+++ src/Day2.hs
@@ -1,4 +1,48 @@
+{-# LANGUAGE DerivingVia #-}
 module Day2 where
 
 program :: String -> IO ()
 program _ = pure ()
+
+data Move = Rock | Paper | Scissors deriving (Eq,Show, Bounded, Enum)
+
+newtype Score = Score Int deriving (Eq, Show, Num, Ord) via Int 
+
+data Match = Match Move Move deriving (Eq,Show)
+
+data Winner = Player1 | Player2 | Draw deriving (Eq,Show)
+
+data Scores = Scores {
+   score1 :: Score, 
+   score2 :: Score 
+   } deriving (Eq,Show)
+
+score :: Move -> Score 
+score = (+ 1) . fromIntegral . fromEnum
+--score Rock = 1
+--score Paper = 2
+--score Scissors = 3
+
+--winnerScore :: Winner -> Score
+--scoreOutcome = (* 3) . fromEnum
+--scoreOutcome Loss = 0 
+--scoreOutcome Draw = 3
+--scoreOutcome Win = 6
+
+parsePlayer1 :: Char -> Maybe Move
+parsePlayer1 'A' = Just Rock 
+parsePlayer1 'B' = Just Paper
+parsePlayer1 'C' = Just Scissors 
+parsePlayer1 _ = Nothing 
+
+parsePlayer2 :: Char -> Maybe Move
+parsePlayer2 'X' = Just Rock 
+parsePlayer2 'Y' = Just Paper
+parsePlayer2 'Z' = Just Scissors 
+parsePlayer2 _ = Nothing 
+
+winner :: Match -> Winner 
+winner (Match m1 m2) | m2 == m1 = Draw
+                     | m2 == succ m1 = Player2
+                     | otherwise = Player1
+
--- test/Day2Spec.hs
+++ test/Day2Spec.hs
@@ -4,12 +4,41 @@ import Day2
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck.Property
+import Test.QuickCheck.Arbitrary
+
+
+instance Arbitrary Move where
+   arbitrary = arbitraryBoundedEnum
+
 
 spec :: Spec
-spec = describe "Simple test" $ do
+spec = describe "Day2: Play rock-paper-scissors" $ do
 
-     it "example-based unit test" $
-        1 `shouldBe` 1
+     it "paper wins over rock" $
+        winner (Match Rock Paper) `shouldBe` Player2
+
+     it "rock wins over scissors" $
+        winner (Match Rock Scissors) `shouldBe` Player1
+
+     it "rock rock is a draw" $
+        winner (Match Rock Rock) `shouldBe` Draw
+
+     it "scissors win over paper " $
+        winner (Match Scissors Paper) `shouldBe` Player1
+
+     prop "always result in draw if the moves are the same for all players" $
+        \m -> winner (Match m m) == Draw
+
+     prop "never result in draw if the moves are not the same for the two players" $
+        \m1 m2  -> m1 /= m2 ==> winner (Match m1 m2) /= Draw
+     
+     prop "winner is commutative" $
+        \m1 m2  -> m1 /= m2 ==> (
+          (winner (Match m1 m2) == Player1  && 
+          winner (Match m2 m1) == Player2)  || 
+           (winner (Match m1 m2) == Player2  && 
+            winner (Match m2 m1) == Player1))
+
 
      prop "property-based unit test" $
         \l -> reverse ( reverse l ) == ( l::[Int])
