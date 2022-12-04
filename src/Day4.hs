eb4ea3d89392e9496421fa2e7bf13c7250a64a2e
Alessandro Candolini <alessandrocandolini@gmail.com>
2022-12-04 05:51:40 +0000

    Day4

--- src/Day4.hs
+++ src/Day4.hs
@@ -1,11 +1,58 @@
 module Day4 where
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import Data.List.Split (splitOn)
+import Data.Maybe
+import Control.Monad
 
 program :: FilePath -> IO ()
-program = (=<<) print . fmap pureProgram . T.readFile
+program = (=<<) printReport . fmap pureProgram . T.readFile
 
-data Report = Report deriving (Eq, Show)
+data Report = Report {
+  part1 :: [Overlap], part2 :: [Overlap] }  deriving (Eq, Show)
+
+solution1 = length . part1
+solution2 = length . part2
+
+printReport r = putStrLn "Solution 1:" >>= \_ -> print (solution1 r) >>= \_ ->  putStrLn " " >>= \_ ->  print (solution2 r)
 
 pureProgram :: T.Text -> Report
-pureProgram = const Report
+pureProgram = uncurry Report . ((,) <$> p1 <*> p2) . fmap overlap . parse where
+    p1 = mfilter (Fully ==)
+    p2 = mfilter (No /=)
+
+parse :: T.Text -> [Round]
+parse = mapMaybe (parseLine .T.unpack) . T.lines
+
+parseLine :: String -> Maybe Round
+parseLine = toRound . mapMaybe parseRange . splitOn "," where
+  toRound :: [Range] -> Maybe Round
+  toRound [h, h2] = Just (Round h h2)
+  toRound _ = Nothing
+
+data Round = Round {
+  range1 :: Range,
+  range2 :: Range } deriving (Eq,Show)
+
+data Range = Range {
+  start :: Int,
+  end :: Int
+  } deriving (Eq,Show)
+
+parseRange :: String -> Maybe Range
+parseRange = toRange . fmap read . splitOn "-" where
+  toRange (s:e:[]) = Just (Range s e)
+  toRange _ = Nothing
+
+
+data Overlap = Fully | Partially | No deriving (Eq, Show)
+
+checkOverlap :: Range -> Range -> Overlap
+checkOverlap (Range a b) (Range c d) | a <= c && d <= b = Fully
+                                         | c <= a && b <= d = Fully
+                                         | a <= c && c <= b = Partially
+                                         | c <= a && a <= d = Partially
+                                         | otherwise = No
+
+overlap :: Round -> Overlap
+overlap = checkOverlap <$> range1 <*> range2
--- test/Day4Spec.hs
+++ test/Day4Spec.hs
@@ -7,14 +7,31 @@ import Test.Hspec.QuickCheck
 import Test.QuickCheck.Property
 import NeatInterpolation
 
+input =
+  [trimming|
+            2-4,6-8
+            2-3,4-5
+            5-7,7-9
+            2-8,3-7
+            6-6,4-6
+            2-6,4-8
+          |]
+
 spec :: Spec
 spec = describe "Day4" $ do
 
-     it "" $
-        1 `shouldBe` 1
+     it "count overlapping" $
+        pureProgram input `shouldBe` (Report [Fully , Fully] [Partially, Fully, Fully, Partially])
 
-     it "" $
-        1 `shouldBe` 1
+     it "overlap single selection" $
+        checkOverlap (Range 5 7) (Range 7 9) `shouldBe` Partially
+
+     it "do not ovelap" $
+        checkOverlap (Range 2 4) (Range 6 8) `shouldBe` No
+
+
+     it "do not ovelap" $
+        checkOverlap (Range 2 3) (Range 4 5) `shouldBe` No
 
      it "" $
         1 `shouldBe` 1
