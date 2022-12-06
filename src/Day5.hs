9c93eddc145da2253179f193c1fe0f627fea3743
Alessandro Candolini <alessandrocandolini@gmail.com>
2022-12-06 23:20:28 +0000

    not working day5

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,6 +1,13 @@
+{-# LANGUAGE DerivingVia #-}
+{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
+{-# HLINT ignore "Redundant bracket" #-}
+
 module Day5 where
+
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import qualified Data.Vector as V
+import Data.Maybe (fromMaybe)
 
 program :: FilePath -> IO ()
 program = (=<<) print . fmap pureProgram . T.readFile
@@ -9,3 +16,56 @@ data Report = Report deriving (Eq, Show)
 
 pureProgram :: T.Text -> Report
 pureProgram = const Report
+
+data Stack a = Empty | NonEmpty a (Stack a) deriving (Eq, Show)
+
+push :: a -> Stack a -> Stack a
+push = NonEmpty
+
+pop :: Stack a -> Maybe (a, Stack a)
+pop Empty = Nothing
+pop (NonEmpty a s) = Just (a, s)
+
+stackSize :: Stack a -> Int
+stackSize Empty = 0
+stackSize (NonEmpty _ s) = 1 + stackSize s
+
+newtype Crate = Crate String deriving (Eq, Show)
+
+newtype Cargo = Cargo {
+     stacks :: V.Vector (Stack Crate) } deriving (Eq, Show)
+
+cargoSize :: Cargo -> Int
+cargoSize = V.length . stacks
+
+content :: Cargo -> Int
+content = sum . fmap stackSize . stacks
+
+newtype Origin = Origin Int deriving (Eq, Show, Num, Ord) via Int
+
+newtype Destination = Destination Int deriving (Eq, Show, Num, Ord) via Int
+
+data Move = Move Origin Destination deriving (Eq, Show)
+
+reverseMove :: Move -> Move
+reverseMove (Move (Origin i) (Destination j)) = Move (Origin j) (Destination i)
+
+cargoPop :: Cargo -> Int -> Maybe (Crate, Cargo)
+cargoPop c i = do
+     s <- (stacks c) V.!? i
+     (a, s') <- pop s
+     pure (a, Cargo $ (stacks c) V.// [(i, s')])
+
+cargoPush :: Cargo -> Int -> Crate -> Cargo
+cargoPush c _ _ = c
+--cargoPush c i a = let
+        --s' = case stacks c V.!? i of
+          --Just s ->  push a s
+          --Nothing -> push a Empty
+       --in
+        --Cargo $ V.update (stacks c) (V.singleton (i, s'))
+
+
+
+rearrange :: Move -> Cargo -> Cargo
+rearrange (Move (Origin o) (Destination d)) c = maybe c (\(a, c') -> cargoPush c' d a) (cargoPop c o)
--- test/Day5Spec.hs
+++ test/Day5Spec.hs
@@ -6,10 +6,36 @@ import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck.Property
 import NeatInterpolation
+import Test.QuickCheck
+import qualified Data.Vector as V
 
 input =
   [trimming|
+             D]
+        [N] [C]
+        [Z] [M] [P]
+         1   2   3
+
+        move 1 from 2 to 1
+        move 3 from 1 to 3
+        move 2 from 2 to 1
+        move 1 from 1 to 2
           |]
+
+instance Arbitrary Move where
+   arbitrary = move <$> arbitrary <*> arbitrary where
+               move a1 a2 = Move (Origin (getPositive a1)) (Destination (getPositive a2))
+
+instance Arbitrary a => Arbitrary (Stack a) where
+   arbitrary = stackFromList <$> arbitrary where
+            stackFromList :: [a] -> Stack a
+            stackFromList = foldr push Empty
+
+instance Arbitrary Crate where
+   arbitrary = Crate <$> arbitrary
+
+instance Arbitrary Cargo where
+   arbitrary = Cargo . V.fromList <$> arbitrary
 
 
 spec :: Spec
@@ -18,6 +44,15 @@ spec = describe "Day 5" $ do
      it "" $
         1 `shouldBe` 1
 
-     prop "" $
-        \l -> reverse ( reverse l ) == ( l::[Int])
+     prop "rearrange is the identity when source and destination coincide" $
+        \m c -> rearrange m c == c
+
+     prop "reversing the move brings the cargo to the original state" $
+        \m c -> rearrange (reverseMove m) (rearrange m c) == c
+
+     prop "size and content of a cargo is left unchanged after an operation" $
+        \m c -> let
+               c' = rearrange m c
+            in
+               (cargoSize c', content c' ) == (cargoSize c , content c)
 
