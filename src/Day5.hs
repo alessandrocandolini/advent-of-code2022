cdcb6f0f73683f97bbb86754dd1208da702a287f
Alessandro Candolini <alessandrocandolini@gmail.com>
2023-11-03 09:59:08 +0000

    working on day 5

--- src/Day5.hs
+++ src/Day5.hs
@@ -1,23 +1,32 @@
+{-# LANGUAGE DeriveFoldable #-}
 {-# LANGUAGE DerivingVia #-}
-{-# OPTIONS_GHC -Wno-unrecognised-pragmas #-}
-{-# HLINT ignore "Redundant bracket" #-}
 
 module Day5 where
 
+import qualified Data.IntMap as M
+import Data.Maybe (fromMaybe)
+import Data.Semigroup
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
-import qualified Data.Vector as V
-import Data.Maybe (fromMaybe)
+import Safe (readMay)
+import Witherable (mapMaybe)
+import Text.Megaparsec
+import Text.Megaparsec.Char
+import Data.Void
+import Data.Functor (($>))
+import Data.Either.Combinators (rightToMaybe)
 
 program :: FilePath -> IO ()
-program = (=<<) print . fmap pureProgram . T.readFile
+program = (=<<) print . fmap (fmap (toAnswer . uncurry (flip logic)) . parseInput) . T.readFile where
+  toAnswer :: [Crate] -> Answer
+  toAnswer = Answer . fmap name
 
-data Report = Report deriving (Eq, Show)
+newtype Answer = Answer String deriving (Eq, Show)
 
-pureProgram :: T.Text -> Report
-pureProgram = const Report
+logic :: [Instruction] -> Cargo a -> [a]
+logic inst = peeks . rearrangeAll (explodeAll inst)
 
-data Stack a = Empty | NonEmpty a (Stack a) deriving (Eq, Show)
+data Stack a = Empty | NonEmpty a (Stack a) deriving (Eq, Show, Foldable)
 
 push :: a -> Stack a -> Stack a
 push = NonEmpty
@@ -26,46 +35,94 @@ pop :: Stack a -> Maybe (a, Stack a)
 pop Empty = Nothing
 pop (NonEmpty a s) = Just (a, s)
 
+peek :: Stack a -> Maybe a
+peek = fmap fst . pop
+
 stackSize :: Stack a -> Int
-stackSize Empty = 0
-stackSize (NonEmpty _ s) = 1 + stackSize s
+stackSize = getSum . foldMap (const 1)
 
-newtype Crate = Crate String deriving (Eq, Show)
+newtype Crate = Crate {name :: Char} deriving (Eq, Show)
 
-newtype Cargo = Cargo {
-     stacks :: V.Vector (Stack Crate) } deriving (Eq, Show)
+newtype Cargo a = Cargo
+  { stacks :: M.IntMap (Stack a)
+  }
+  deriving (Eq, Show)
 
-cargoSize :: Cargo -> Int
-cargoSize = V.length . stacks
+peeks :: Cargo a -> [a]
+peeks = mapMaybe peek . M.elems . stacks
 
-content :: Cargo -> Int
-content = sum . fmap stackSize . stacks
+data Move = Move {from :: Int, to :: Int} deriving (Eq, Show)
 
-newtype Origin = Origin Int deriving (Eq, Show, Num, Ord) via Int
+data Instruction = Instruction Int Int Int deriving (Eq, Show)
 
-newtype Destination = Destination Int deriving (Eq, Show, Num, Ord) via Int
+explode :: Instruction -> [Move]
+explode (Instruction n f t) = replicate n (Move f t)
 
-data Move = Move Origin Destination deriving (Eq, Show)
+explodeAll :: [Instruction] -> [Move]
+explodeAll = (=<<) explode
 
-reverseMove :: Move -> Move
-reverseMove (Move (Origin i) (Destination j)) = Move (Origin j) (Destination i)
+cargoPop :: Int -> Cargo a -> Maybe (a, Cargo a)
+cargoPop k c = do
+  s <- M.lookup k (stacks c)
+  (a, s') <- pop s
+  pure (a, update k s' c)
+ where
+  update :: Int -> Stack a -> Cargo a -> Cargo a
+  update k1 s1 = Cargo . insertOrDelete k1 s1 . stacks
+  insertOrDelete :: Int -> Stack a -> M.IntMap (Stack a) -> M.IntMap (Stack a)
+  insertOrDelete k1 Empty = M.delete k1
+  insertOrDelete k1 s1@(NonEmpty _ _) = M.insert k1 s1
 
-cargoPop :: Cargo -> Int -> Maybe (Crate, Cargo)
-cargoPop c i = do
-     s <- (stacks c) V.!? i
-     (a, s') <- pop s
-     pure (a, Cargo $ (stacks c) V.// [(i, s')])
+cargoPush :: Int -> a -> Cargo a -> Cargo a
+cargoPush k a = Cargo . M.alter (insertOrAppend a) k . stacks
+ where
+  insertOrAppend :: a -> Maybe (Stack a) -> Maybe (Stack a)
+  insertOrAppend a1 = Just . push a1 . fromMaybe Empty
 
-cargoPush :: Cargo -> Int -> Crate -> Cargo
-cargoPush c _ _ = c
---cargoPush c i a = let
-        --s' = case stacks c V.!? i of
-          --Just s ->  push a s
-          --Nothing -> push a Empty
-       --in
-        --Cargo $ V.update (stacks c) (V.singleton (i, s'))
+rearrange :: Move -> Cargo a -> Cargo a
+rearrange (Move k1 k2) c = fromMaybe c $ do
+  (a, c') <- cargoPop k1 c
+  pure $ cargoPush k2 a c'
 
+rearrangeAll :: [Move] -> Cargo a -> Cargo a
+rearrangeAll = appEndo . foldMap (Endo . rearrange)
 
+parseInstruction :: String -> Maybe Instruction
+parseInstruction input = case words input of
+  ["move", x, "from", y, "to", z] -> do
+    x' <- readMay x
+    y' <- readMay y
+    z' <- readMay z
+    pure (Instruction x' y' z')
+  _ -> Nothing
 
-rearrange :: Move -> Cargo -> Cargo
-rearrange (Move (Origin o) (Destination d)) c = maybe c (\(a, c') -> cargoPush c' d a) (cargoPop c o)
+parseInput :: T.Text -> Maybe (Cargo Crate, [Instruction])
+parseInput input = case T.splitOn "\n\n" input of
+   [p1, p2] -> (,) <$> parseCargo p1  <*> Just (parseInstructions p2)
+   _ -> Nothing
+
+parseInstructions :: T.Text -> [Instruction]
+parseInstructions = mapMaybe (parseInstruction . T.unpack) . T.lines
+
+type Parser a = Parsec Void T.Text a
+
+crateP :: Parser Crate
+crateP = Crate <$> between (char '[') (char ']') letterChar
+
+optionalCrateP :: Parser (Maybe Crate)
+optionalCrateP = (Just <$> crateP) <|> emptyP where
+   emptyP = count 3 spaceChar $> Nothing
+
+cratesP :: Parser [Maybe Crate]
+cratesP = optionalCrateP `sepBy` spaceChar
+
+allCratesP :: Parser [[Maybe Crate]]
+allCratesP = cratesP `sepEndBy` eol <* eof
+
+parseCrates :: T.Text -> Maybe [[Maybe Crate]]
+parseCrates = rightToMaybe . parse allCratesP ""
+
+parseCargo :: T.Text -> Maybe (Cargo Crate)
+parseCargo _ = Just $ Cargo $ M.empty
+                      -- takeWhile (not . null) . iterate (drop 4) . tail .
+                      --fmap (dropWhile (== ' ') . trasponse . fmap T.unpack . init . T.lines
--- test/Day5Spec.hs
+++ test/Day5Spec.hs
@@ -1,17 +1,25 @@
 {-# LANGUAGE QuasiQuotes #-}
+{-# LANGUAGE OverloadedLists #-}
+{-# LANGUAGE TypeFamilies #-}
 module Day5Spec where
 
 import Day5
+import Data.Maybe (isNothing, isJust)
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck.Property
 import NeatInterpolation
 import Test.QuickCheck
-import qualified Data.Vector as V
+import qualified Data.IntMap as M
+import GHC.Exts (IsList (fromList, toList), Item)
+import Data.List (unfoldr, transpose)
+import Data.Semigroup
+import qualified Data.Text as T
+import Data.List.Split (splitOn)
 
 input =
   [trimming|
-             D]
+            [D]
         [N] [C]
         [Z] [M] [P]
          1   2   3
@@ -22,37 +30,144 @@ input =
         move 1 from 1 to 2
           |]
 
+inputPart1 = [text|
+    [D]
+[N] [C]
+[Z] [M] [P]
+ 1   2   3
+|]
+
+stackFromList :: [a] -> Stack a
+stackFromList = foldr push Empty
+
+stackToList :: Stack a -> [a]
+stackToList = unfoldr pop
+
+instance IsList (Stack a) where
+  type Item (Stack a) = a
+  fromList = stackFromList
+  toList = stackToList
+
+instance IsList (Cargo a) where
+  type Item (Cargo a) = (Int, Stack a)
+  fromList = Cargo . fromList
+  toList = toList . stacks
+
 instance Arbitrary Move where
-   arbitrary = move <$> arbitrary <*> arbitrary where
-               move a1 a2 = Move (Origin (getPositive a1)) (Destination (getPositive a2))
+   arbitrary = Move <$> arbitrary <*> arbitrary
 
 instance Arbitrary a => Arbitrary (Stack a) where
-   arbitrary = stackFromList <$> arbitrary where
-            stackFromList :: [a] -> Stack a
-            stackFromList = foldr push Empty
+   arbitrary = stackFromList <$> arbitrary
 
 instance Arbitrary Crate where
    arbitrary = Crate <$> arbitrary
 
-instance Arbitrary Cargo where
-   arbitrary = Cargo . V.fromList <$> arbitrary
+instance Arbitrary a => Arbitrary (Cargo a) where
+   arbitrary = Cargo . M.fromList <$> listOf ((,) <$> choose (-20, 40) <*> arbitrary)
 
+reverseMove (Move k1 k2) = Move k2 k1
+
+cargoSize :: Cargo a -> Int
+cargoSize = getSum . foldMap (Sum . stackSize) . M.elems . stacks
 
 spec :: Spec
 spec = describe "Day 5" $ do
 
-     it "" $
-        1 `shouldBe` 1
+     it "stack from list" $
+       ["A", "B", "C"] `shouldBe` push "A" ( push "B" ( push "C" Empty))
+
+     it "stack to list" $
+       toList (push "A" ( push "B" ( push "C" Empty))) `shouldBe` ["A", "B", "C"]
+
+     it "size of empty stack" $
+       stackSize [] `shouldBe` 0
+
+     it "size of nonempty stack" $
+       stackSize ["A", "B", "C"] `shouldBe` 3
+
+     it "cargo from list" $
+       [(1, ["A", "B", "C"]), (2, ["D", "E"]), (3, [])] `shouldBe` Cargo (M.fromList [(1, push "A" (push "B" (push "C" Empty))), (2, push "D" (push "E" Empty)), (3, Empty)])
+
+     it "perform a valid re-arrange" $ let
+        cargo = [(1, ["N", "Z"]), (2, ["D", "C", "M"]), (3, ["P"])]
+        expected = [(1, ["D", "N", "Z"]), (2, ["C", "M"]), (3, ["P"])]
+      in
+        rearrange (Move 2 1) cargo `shouldBe` expected
+
+     it "perform multiple valid re-arranges" $ let
+        cargo = [(1, ["N", "Z"]), (2, ["D", "C", "M"]), (3, ["P"])]
+        moves = [Move 2 1, Move 1 3, Move 1 3, Move 1 3, Move 2 1, Move 2 1, Move 1 2]
+        expected = [(1, ["C"]), (2, ["M"]), (3, ["Z", "N", "D", "P"])]
+      in
+        rearrangeAll moves cargo `shouldBe` expected
+
+     it "perform an invalid re-arrange" $ let
+        cargo = [(1, ["N", "Z"]), (2, ["D", "C", "M"]), (3, [])]
+      in
+        rearrange (Move 3 1) cargo `shouldBe` cargo
+
+     it "peaks returns the top elements of each stack" $ let
+        cargo = [(1, ["N", "Z"]), (2, ["D", "C", "M"]), (3, ["P"])]
+      in
+        peeks cargo `shouldBe` ["N", "D", "P"]
+
+     it "peaks ignores empty stacks when returning the top elements of each stack" $ let
+        cargo = [(1, ["N", "Z"]), (2, ["D", "C", "M"]), (3, []), (4, ["P"])]
+      in
+        peeks cargo `shouldBe` ["N", "D", "P"]
+
+
+     it "example of composing move and its reverse" $  let
+              m = Move 0 (-1)
+              c = Cargo $ fromList [(0,['a', 'b'])]
+         in
+              (rearrange (reverseMove m) . rearrange m) c `shouldBe` c
 
      prop "rearrange is the identity when source and destination coincide" $
-        \m c -> rearrange m c == c
+        \k c -> rearrange (Move k k) c == (c :: Cargo Char)
 
      prop "reversing the move brings the cargo to the original state" $
-        \m c -> rearrange (reverseMove m) (rearrange m c) == c
+        \m c ->
+           (rearrange (reverseMove m) . rearrange m ) c == c ||
+           (rearrange (reverseMove m) . rearrange m ) c == rearrange (reverseMove m) (c :: Cargo Char)
 
-     prop "size and content of a cargo is left unchanged after an operation" $
-        \m c -> let
-               c' = rearrange m c
-            in
-               (cargoSize c', content c' ) == (cargoSize c , content c)
+     prop "size is left unchanged after an operation" $
+        \m c -> cargoSize (rearrange m c)  == cargoSize (c :: Cargo Char)
+
+     it "parseInstruction parse correctly a valid text" $
+        parseInstruction "move 1 from 7 to 4" `shouldBe` Just (Instruction 1 7 4)
+
+     it "parseInstructions parse correctly a sequence of valid text" $
+        parseInstructions [trimming|
+          move 1 from 2 to 1
+          move 3 from 1 to 3
+          move 2 from 2 to 1
+          move 1 from 1 to 2
+         |] `shouldBe` [Instruction 1 2 1, Instruction 3 1 3, Instruction 2 2 1 , Instruction 1 1 2]
+
+     prop "parseInstruction parse correctly any valid text" $
+        \ a b c ->
+        let
+          a' = getPositive a
+          b' = getPositive b
+          c' = getPositive c
+          s ="move " ++ show a' ++ " from " ++ show b' ++ " to " ++ show c'
+        in
+          parseInstruction s `shouldBe` Just (Instruction a' b' c')
+
+     it "parse crates" $
+      let
+         s :: T.Text
+         s = [text|     [D]     \n [N] [C]     \n [Z] [M] [P] |]
+      in
+         parseCrates s `shouldBe` Just [[Nothing, Just (Crate 'D'), Nothing],
+                [Just (Crate 'N'), Just (Crate 'C'), Nothing],
+                [Just (Crate 'Z'), Just (Crate 'M'), Just (Crate 'P')]]
+
+     it "parse example input" $
+        let
+          cargo = [(1, [Crate 'N', Crate 'Z']), (2, [Crate 'D', Crate 'C', Crate 'M']), (3, [Crate 'P'])]
+          instructions = [Instruction 1 2 1, Instruction 3 1 3, Instruction 2 2 1 , Instruction 1 1 2]
+        in
+          parseInput input `shouldBe` Just (cargo, instructions)
 
