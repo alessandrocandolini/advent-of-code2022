4e5be375d428c4e328bfa7a36e3328a2a8286649
Alessandro Candolini <alessandrocandolini@gmail.com>
2023-11-04 12:30:03 +0000

    wip day 7

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,11 +1,39 @@
+{-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DerivingVia #-}
+
 module Day7 where
+
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import GHC.Exts (IsString)
+import Data.Semigroup
+import Data.Foldable
 
 program :: FilePath -> IO ()
 program = (=<<) print . fmap logic . T.readFile
 
 data Answer = Answer deriving (Eq, Show)
 
+newtype DirectoryName = DirectoryName String
+  deriving (Eq, Show)
+  deriving (IsString) via String
+
+newtype FileName = FileName String
+  deriving (Eq, Show)
+  deriving (IsString) via String
+
+newtype Size = Size Int
+  deriving (Eq,Show)
+  deriving (Ord, Num, Semigroup, Monoid) via (Sum Int)
+
+data FileSystem a
+  = File FileName a
+  | Directory DirectoryName [FileSystem a]
+  deriving (Eq, Show, Foldable)
+
 logic :: T.Text -> Answer
 logic = const Answer
+
+du :: Monoid m => FileSystem m -> m
+du = fold
+
--- test/Day7Spec.hs
+++ test/Day7Spec.hs
@@ -7,17 +7,26 @@ import Test.Hspec.QuickCheck
 import Test.QuickCheck.Property
 import NeatInterpolation
 
-input =
-  [trimming|
-          |]
-
-
 spec :: Spec
-spec = describe "Day N" $ do
+spec = describe "Day 7" $ do
 
-     it "" $
-        1 `shouldBe` 1
+     it "total size of a filesystem" $ let
+       filesystem = Directory "/"
+        [ File "file1.txt" 100
+        , File "file2.txt" 200
+        , Directory "folder1"
+            [ File "file3.txt" 150
+            , File "file4.txt" 300
+            , Directory "folder3"
+              [ File "file5.txt" 200
+              , File "file6.txt" 300
+              ]
+            ]
+        , Directory "folder2"
+            [ File "file7.txt" 250
+            ]
+        ]
+      in
+        du filesystem `shouldBe` (1500 :: Size)
 
-     prop "" $
-        \l -> reverse ( reverse l ) == ( l::[Int])
 
