0450b9f4917dd92ee03a9241c626110c46a63600
Alessandro Candolini <alessandrocandolini@gmail.com>
2023-11-08 02:07:39 +0000

    day 7 wip

--- src/Day7.hs
+++ src/Day7.hs
@@ -1,4 +1,5 @@
 {-# LANGUAGE DeriveFoldable #-}
+{-# LANGUAGE DeriveFunctor #-}
 {-# LANGUAGE DerivingVia #-}
 
 module Day7 where
@@ -8,6 +9,11 @@ import qualified Data.Text.IO as T
 import GHC.Exts (IsString)
 import Data.Semigroup
 import Data.Foldable
+import qualified Data.List.NonEmpty as N
+import Data.List.NonEmpty (NonEmpty)
+import Data.Fix
+import Text.PrettyPrint (Doc)
+import qualified Text.PrettyPrint as P
 
 program :: FilePath -> IO ()
 program = (=<<) print . fmap logic . T.readFile
@@ -26,14 +32,39 @@ newtype Size = Size Int
   deriving (Eq,Show)
   deriving (Ord, Num, Semigroup, Monoid) via (Sum Int)
 
-data FileSystem a
-  = File FileName a
-  | Directory DirectoryName [FileSystem a]
-  deriving (Eq, Show, Foldable)
+data FileSystemF a
+  = FileF FileName Size
+  | DirectoryF DirectoryName [a]
+  deriving (Eq, Show, Foldable, Functor)
+
+type FileSystem = Fix FileSystemF
+
+file :: FileName -> Size -> FileSystem
+file name = Fix . FileF name
+
+directory :: DirectoryName -> [FileSystem] -> FileSystem
+directory name = Fix . DirectoryF name
 
 logic :: T.Text -> Answer
 logic = const Answer
 
-du :: Monoid m => FileSystem m -> m
-du = fold
+duAlgebra :: FileSystemF Size -> Size
+duAlgebra (FileF _ size) = size
+duAlgebra (DirectoryF _ sizes) = sum sizes
 
+du :: FileSystem -> Size
+du = cata duAlgebra
+
+duDir :: FileSystem -> [(DirectoryName, Size)]
+duDir = undefined
+
+pretty :: FileSystemF Doc -> Doc
+pretty (FileF (FileName name) (Size size)) = P.text name <> P.space <> P.int size
+pretty (DirectoryF _ docs) = P.cat docs
+
+display :: FileSystem -> String
+display = P.render . cata pretty
+
+
+-- pretty print
+-- exclude files
--- test/Day7Spec.hs
+++ test/Day7Spec.hs
@@ -6,27 +6,45 @@ import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck.Property
 import NeatInterpolation
+import qualified Data.Text as T
+
+filesystem = directory "/"
+        [ file "file1.txt" 100
+        , file "file2.txt" 200
+        , directory "folder1"
+            [ file "file3.txt" 150
+            , file "file4.txt" 300
+            , directory "folder3"
+              [ file "file5.txt" 200
+              , file "file6.txt" 300
+              ]
+            ]
+        , directory "folder2"
+            [ file "file7.txt" 250
+            ]
+        ]
 
 spec :: Spec
 spec = describe "Day 7" $ do
 
-     it "total size of a filesystem" $ let
-       filesystem = Directory "/"
-        [ File "file1.txt" 100
-        , File "file2.txt" 200
-        , Directory "folder1"
-            [ File "file3.txt" 150
-            , File "file4.txt" 300
-            , Directory "folder3"
-              [ File "file5.txt" 200
-              , File "file6.txt" 300
-              ]
-            ]
-        , Directory "folder2"
-            [ File "file7.txt" 250
-            ]
-        ]
-      in
-        du filesystem `shouldBe` (1500 :: Size)
+     it "total size of a filesystem" $
+        du filesystem `shouldBe` 1500
+
+     it "total size of a filesystem" $
+        duDir filesystem `shouldBe` [("/", 1500), ("folder1", 950), ("folder3", 500), ("folder2", 250)]
+
+     it "print directory and file structure" $
+        display filesystem `shouldBe` T.unpack [trimming|
+           /
+           |-- file1.txt
+           |-- file2.txt
+           |-- folder1
+           |-- |-- file3.txt
+           |-- |-- file4.txt
+           |-- |-- folder3
+           |-- |-- |-- file5.txt
+           |-- |-- |-- file6.txt
+           |-- folder2
+           |-- |-- file7.txt|]
 
 
