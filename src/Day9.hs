cb7de9965b5313189065e5b2d2a07f2556f3cc1a
Alessandro Candolini <alessandrocandolini@gmail.com>
2023-11-20 01:22:21 +0000

    day 9 part 1

--- src/Day9.hs
+++ src/Day9.hs
@@ -1,11 +1,79 @@
 module Day9 where
+
+import Data.Either (fromRight)
+import Data.Either.Combinators (rightToMaybe)
+import Data.Functor (($>))
+import Data.List (nub)
+import Data.Maybe (fromMaybe)
 import qualified Data.Text as T
 import qualified Data.Text.IO as T
+import Data.Void
+import Text.Megaparsec
+import Text.Megaparsec.Char (char)
+import Text.Megaparsec.Char.Lexer (decimal)
 
 program :: FilePath -> IO ()
 program = (=<<) print . fmap logic . T.readFile
 
-data Answer = Answer deriving (Eq, Show)
+data Answer = Answer Int deriving (Eq, Show)
 
 logic :: T.Text -> Answer
-logic = const Answer
+logic = Answer . length . nub . fmap ropeTail . evolveRope initialRope . explodeAll . parseInstruction
+
+initialRope :: Rope
+initialRope = Rope (Position 0 0) (Position 0 0)
+
+data Position = Position Int Int deriving (Eq, Show)
+
+data Direction = Up | Down | LeftWard | RightWard deriving (Eq, Show)
+
+data Rope = Rope {ropeHead :: Position, ropeTail :: Position} deriving (Eq, Show)
+
+move :: Direction -> Position -> Position
+move Up (Position x y) = Position x (y + 1)
+move Down (Position x y) = Position x (y - 1)
+move LeftWard (Position x y) = Position (x - 1) y
+move RightWard (Position x y) = Position (x + 1) y
+
+chebyshev :: Position -> Position -> Int
+chebyshev (Position x1 y1) (Position x2 y2) = max (abs (x1 - x2)) (abs (y1 - y2))
+
+moveClose :: Position -> Position -> Position
+moveClose h t | chebyshev h t <= 1 = t
+moveClose (Position xH yH) (Position xT yT) = Position (xT + signum (xH - xT)) (yT + signum (yH - yT))
+
+moveRope :: Rope -> Direction -> Rope
+moveRope (Rope h t) direction = Rope newH newT
+ where
+  newH = move direction h
+  newT = moveClose newH t
+
+evolveRope :: Rope -> [Direction] -> [Rope]
+evolveRope = scanl moveRope
+
+data Instruction = Instruction Direction Int deriving (Eq, Show)
+
+explode :: Instruction -> [Direction]
+explode (Instruction direction times) = replicate times direction
+
+explodeAll :: [Instruction] -> [Direction]
+explodeAll = (=<<) explode
+
+type Parser = Parsec Void T.Text
+
+directionP :: Parser Direction
+directionP =
+  char 'R'
+    $> RightWard
+    <|> char 'L'
+    $> LeftWard
+    <|> char 'U'
+    $> Up
+    <|> char 'D'
+    $> Down
+
+instructionP :: Parser Instruction
+instructionP = Instruction <$> (directionP <* char ' ') <*> decimal
+
+parseInstruction :: T.Text -> [Instruction]
+parseInstruction = fromRight [] . traverse (parse instructionP "") . T.lines
--- test/Day9Spec.hs
+++ test/Day9Spec.hs
@@ -1,23 +1,63 @@
 {-# LANGUAGE QuasiQuotes #-}
+
 module Day9Spec where
 
 import Day9
+import NeatInterpolation
 import Test.Hspec
 import Test.Hspec.QuickCheck
 import Test.QuickCheck.Property
-import NeatInterpolation
-
-input =
-  [trimming|
-          |]
-
 
 spec :: Spec
-spec = describe "Day N" $ do
+spec = describe "Day 9" $ do
+  it "parse instruction"
+    $ parseInstruction
+      [trimming|
+          L 1
+          U 3
+          R 1
+          D 2
+          |]
+    `shouldBe` [ Instruction LeftWard 1
+               , Instruction Up 3
+               , Instruction RightWard 1
+               , Instruction Down 2
+               ]
 
-     it "" $
-        1 `shouldBe` 1
-
-     prop "" $
-        \l -> reverse ( reverse l ) == ( l::[Int])
-
+  it "evolveRope"
+    $ (fmap ropeTail . evolveRope initialRope . explodeAll)
+      [ Instruction RightWard 4
+      , Instruction Up 4
+      , Instruction LeftWard 3
+      , Instruction Down 1
+      , Instruction RightWard 4
+      , Instruction Down 1
+      , Instruction LeftWard 5
+      , Instruction RightWard 2
+      ]
+    `shouldBe` [ Position 0 0
+               , Position 0 0
+               , Position 1 0
+               , Position 2 0
+               , Position 3 0
+               , Position 3 0
+               , Position 4 1
+               , Position 4 2
+               , Position 4 3
+               , Position 4 3
+               , Position 3 4
+               , Position 2 4
+               , Position 2 4
+               , Position 2 4
+               , Position 2 4
+               , Position 3 3
+               , Position 4 3
+               , Position 4 3
+               , Position 4 3
+               , Position 4 3
+               , Position 3 2
+               , Position 2 2
+               , Position 1 2
+               , Position 1 2
+               , Position 1 2
+               ]
